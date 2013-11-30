d08f1eda02a1059777c8219aba7ef46cb539a811
sunus <sunuslee@gmail.com>
2013-11-30 22:04:35 +0800

    finish local server
    Signed-off-by: sunus <sunuslee@gmail.com>

--- client/iac.py
+++ client/iac.py
@@ -2,6 +2,37 @@
 # encoding=utf-8
 
 import sys
+import socket
+from iac_config import iac_config
+
+def login(username, password):
+    return True
 
 def main():
-    
+    username = iac_config['username']
+    password = iac_config['password']
+    host = ''
+    port = iac_config['port']
+    size = 8192
+    backlog = 5
+
+    if login(username, password) is not True:
+        print "Can't login in right now."
+        return -1
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host,port))
+    s.listen(backlog)
+    old_text = ''
+    new_text = ''
+    while True:
+        client, address = s.accept()
+        data = client.recv(size)
+        if data:
+            data = data.split('\r\n\r\n')[1]
+            print 'Got data:'
+            print data
+        client.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ client/iac_config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+iac_config = {
+    'username': 'sunus',
+    
+    'password': 'password',
+
+    'projects': ['~/MyProjects/i-am-coding',],
+
+    'port': 7375,
+}
